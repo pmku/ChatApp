02c396c149a6f5ea5b7d722cb5efe364b316dcbe
pmku <37alperen.kurt@gmail.com>
2022-12-30 15:20:27 +0300

    one messy addition

--- servertest.c
+++ servertest.c
@@ -4,31 +4,59 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<string.h>
+#include<signal.h>
 
 #define PORT 17137 //Port that server is going to use
 
-/*Clientside will have a selector menu that lists
-available servers(localhost and one that I'll be hosting)
-*/
+static int client_index = 0; //Will store id for the clients
+
+//Double ll to hold client data
+struct client{
+	int c_index;
+	struct sockaddr_in c_address;
+	char * c_username;
+	struct client *nextc;
+	struct client *prevc;
+};
 
 int sockfd, addrlen; //Socket file descriptor and address length
 struct sockaddr_in address; //*_in there represents inet IPV4
 
-int acceptsock(){
+/*Clientside will have a selector menu that lists
+  available servers(localhost and one that I'll be hosting)
+  */
+/*void quithandler(){
+//Loop below will close connections with every client
+struct client *c_temp = serverclient; //Serverclient pointer will hold the main chatroom(group one)
+while(c_temp->nextc == NULL){
+close(c_address);
+c_temp=c_temp->nextc;
+free(c_temp->prevc);
+}
+*/
+
+int acceptsock(){ //Acceptsock will be called when a new client connects
 	int newsock;
 	if ((newsock = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+		fprintf(stderr,"\nError while accepting connection\n");
+		exit(EXIT_FAILURE);
+	}
 	return newsock;
 }
 
 void mockread(){
-	int sock = acceptsock();
+	int sock;
 	char buff[1024] = {0};
 	while(1){
+		sleep(1);
+		sock=acceptsock();
 		read(sock, buff, 1024);
-		fprintf(stdout,"%s\n", buff);
+		if(buff[0] != 0){
+			system("clear");
+			fprintf(stdout,"%s\n", buff);
+			fprintf(stdout,"%d", sock);
+		}
+		buff[0] = 0;
 	}
 }
 int main(int argc, char *argv[]){
@@ -40,43 +68,43 @@ int main(int argc, char *argv[]){
 	   SOCK_STREAM defines connection type as TCP
 	   SOCK_DGRAM is for UDP which I won't use
 	   Protocol 0 is for IP
-	*/
+	   */
 
-//Failure check for socket
+	//Failure check for socket
 	if (sockfd < 0){
 		fprintf(stderr,"\nSocket creation has failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-//Setsocketopt in case the address in use
+	//Setsocketopt in case the address in use
 	int opt = 1;
-	 if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
 		fprintf(stderr,"\nSocketopt could not be set\n");
 		exit(EXIT_FAILURE);
-	    }
+	}
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
-//Final, binding section
+	//Final, binding section
 	if (bind(sockfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
 		fprintf(stderr,"\nSocket binding has failed\n");
 		exit(EXIT_FAILURE);
-	    }
+	}
 
-//Put the server socket in listening mode. 5 is the max length of the queue
+	//Put the server socket in listening mode. 5 is the max length of the queue
 	if (listen(sockfd, 5) < 0) {
 		fprintf(stderr,"\nSocket can't be listened\n");
 		exit(EXIT_FAILURE);
 	}
-/*Server is set up at this point. To process request from clients;
-	if ((new_socket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-Is going to be present in each thread before read (valread = read(new_socket, buffer, 1024);)
-*/
+	/*Server is set up at this point. To process request from clients;
+	  if ((new_socket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+	  perror("accept");
+	  exit(EXIT_FAILURE);
+	  }
+	  Is going to be present in each thread before read (valread = read(new_socket, buffer, 1024);)
+	  */
 	mockread();
 	return 0;
-	}
+}
 
